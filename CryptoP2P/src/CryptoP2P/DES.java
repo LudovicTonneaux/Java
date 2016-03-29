2fcf570cd9dadf64b6f9fcf34fabd89f60ba0c7d
Samy Coenen <samy.coenen@hotmail.com>
2016-03-29 20:49:23 +0200

    DES Class added with static methodes to encrypt,decrypt and write to file

---
+++ CryptoP2P/src/CryptoP2P/DES.java
@@ -0,0 +1,63 @@
+package CryptoP2P;
+
+import javax.crypto.*;
+import javax.crypto.spec.DESKeySpec;
+import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+/**
+ * Created by Samy Coenen on 29/03/2016.
+ */
+public class DES {
+    public static String Encrypt(String password, String text) {
+        return null;
+    }
+
+    public static void Encrypt(String password, FileInputStream sourceFile, FileOutputStream locationFile) {
+        try {
+            EncryptOrDecrypt(password, Cipher.ENCRYPT_MODE, sourceFile, locationFile);
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println(ex.getMessage());
+        } catch (InvalidKeySpecException ex) {
+            System.out.println(ex.getMessage());
+        } catch (NoSuchPaddingException ex) {
+            System.out.println(ex.getMessage());
+        } catch (InvalidKeyException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+    private static void EncryptOrDecrypt(String password, int mode, InputStream is, OutputStream os) throws NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException, NoSuchPaddingException, IOException {
+        DESKeySpec dks = new DESKeySpec(password.getBytes());
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
+        SecretKey desKey = skf.generateSecret(dks);
+        Cipher cipher = Cipher.getInstance("DES"); // DES/ECB/PKCS5Padding for SunJCE
+
+        if (mode == Cipher.ENCRYPT_MODE) {
+            cipher.init(Cipher.ENCRYPT_MODE, desKey);
+            CipherInputStream cis = new CipherInputStream(is, cipher);
+            Copy(cis, os);
+        } else if (mode == Cipher.DECRYPT_MODE) {
+            cipher.init(Cipher.DECRYPT_MODE, desKey);
+            CipherOutputStream cos = new CipherOutputStream(os, cipher);
+            Copy(is, cos);
+        }
+    }
+
+    private static void Copy(InputStream is, OutputStream os) throws IOException {
+        byte[] bytes = new byte[64];
+        int numBytes;
+        while ((numBytes = is.read(bytes)) != -1) {
+            os.write(bytes, 0, numBytes);
+        }
+        os.flush();
+        os.close();
+        is.close();
+    }
+
+}
--- CryptoP2P/src/CryptoP2P/RSA.java
+++ CryptoP2P/src/CryptoP2P/RSA.java
@@ -1,15 +1,16 @@
 package CryptoP2P;
 
+import javax.crypto.Cipher;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.math.BigInteger;
-import java.security.Key;
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
+import java.security.*;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
+
+import static sun.security.x509.CertificateAlgorithmId.ALGORITHM;
 
 /**
  * Created by Samy Coenen on 29/03/2016.
@@ -39,18 +40,71 @@ public class RSA {
     }
 
     public static void saveToFile(String fileName, BigInteger mod, BigInteger exp) {
-        ObjectOutputStream oout;
-        boolean ooutMade = false;
+        ObjectOutputStream oout = null;
         try {
             oout = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)));
-            ooutMade = true;
             oout.writeObject(mod);
             oout.writeObject(exp);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         } finally {
-            if (ooutMade == true)
-                oout.close();
+            if (oout != null) {
+                try {
+                    oout.close();
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
         }
     }
+
+    /**
+     * Encrypt the plain text using public key.
+     *
+     * @param text : original plain text
+     * @param key  :The public key
+     * @return Encrypted text
+     * @throws java.lang.Exception
+     */
+    public static byte[] encrypt(String text, PublicKey key) {
+        byte[] cipherText = null;
+        try {
+            // get an RSA cipher object and print the provider
+            final Cipher cipher = Cipher.getInstance(ALGORITHM);
+            // encrypt the plain text using the public key
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            cipherText = cipher.doFinal(text.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cipherText;
+    }
+
+    /**
+     * Decrypt text using private key.
+     *
+     * @param text :encrypted text
+     * @param key  :The private key
+     * @return plain text
+     * @throws java.lang.Exception
+     */
+    public static String decrypt(byte[] text, PrivateKey key) {
+        byte[] dectyptedText = null;
+        try {
+            // get an RSA cipher object and print the provider
+            final Cipher cipher = Cipher.getInstance(ALGORITHM);
+
+            // decrypt the text using the private key
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            dectyptedText = cipher.doFinal(text);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return new String(dectyptedText);
+    }
+
 }
