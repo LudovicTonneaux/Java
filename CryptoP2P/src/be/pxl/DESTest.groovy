15f442bd03934f7dff165bfd892121c428ce7656
Samy Coenen <samy.coenen@hotmail.com>
2016-03-31 13:28:54 +0200

    testing with groovy, included with intellij

---
+++ CryptoP2P/src/be/pxl/DESTest.groovy
@@ -0,0 +1,13 @@
+package be.pxl
+/**
+ * Created by Buzz Lightyear on 30/03/2016.
+ */
+class DESTest extends GroovyTestCase {
+    void FileExists() {
+        assertEquals("Is the file found", true,)
+    }
+
+    void testEncrypt1() {
+
+    }
+}
