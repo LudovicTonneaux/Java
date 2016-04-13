67504ef2eb76a89da96b9479a0fb7bc1bcaad20c
Samy Coenen <samy.coenen@hotmail.com>
2016-04-14 00:01:09 +0200

    using built in testing with groovy

--- CryptoP2P/src/be/pxl/DESTest.groovy
+++ CryptoP2P/src/be/pxl/DESTest.groovy
@@ -3,11 +3,12 @@ package be.pxl
  * Created by Buzz Lightyear on 30/03/2016.
  */
 class DESTest extends GroovyTestCase {
+
     void FileExists() {
-        assertEquals("Is the file found", true,)
+        assertEquals("Is the file found", true, true)
     }
 
     void testEncrypt1() {
-
+        assertEquals("Is the file encrypted", true, true)
     }
 }
