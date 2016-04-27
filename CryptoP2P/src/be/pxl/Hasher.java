6d97bd22b2a36f5aa38f02ae31dc6212625f84ab
Samy Coenen <samy.coenen@hotmail.com>
2016-04-27 13:14:22 +0200

    removed comments

--- CryptoP2P/src/be/pxl/Hasher.java
+++ CryptoP2P/src/be/pxl/Hasher.java
@@ -16,13 +16,8 @@ public class Hasher {
             md.update(dataBytes, 0, nread);
         }
         byte[] mdbytes = md.digest();
-        /*convert the byte to hex format method 1
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < mdbytes.length; i++) {
-            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
-        }
-        System.out.println("Hex format : " + sb.toString());*/
-        //convert the byte to hex format method 2
+
+        //convert the byte to hex format m
         StringBuffer hexString = new StringBuffer();
         for (int i = 0; i < mdbytes.length; i++) {
             hexString.append(Integer.toHexString(0xFF & mdbytes[i]));
