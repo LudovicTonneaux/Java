5d275a7a6505c9f7c3744a66e5a9c4ca476b0e11
samy <samy.coenen@hotmail.com>
2016-04-28 08:21:41 +0200

    created another server class for localhost because putting everything in 1 server class was unnecessarily complicated

--- CryptoP2P/src/be/pxl/Client.java
+++ CryptoP2P/src/be/pxl/Client.java
@@ -13,19 +13,7 @@ import java.nio.file.Files;
 public class Client {
 
     public static void Send(String filePath, String ip) {
-        /* Socket sock = new Socket("127.0.0.1", 13501);
-        byte[] mybytearray = new byte[1024];
-        String filePath = new File("").getAbsolutePath();
-        filePath.concat("s.txt");
-        File file = new File(filePath);
-        System.out.println(file.exists());
-        InputStream is = sock.getInputStream();
-        FileOutputStream fos = new FileOutputStream(filePath);
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
-        int bytesRead = is.read(mybytearray, 0, mybytearray.length);
-        bos.write(mybytearray, 0, bytesRead);
-        bos.close();
-        sock.close();*/
+
         try {
             String msg = "Iam waiting for message";
             Socket s = new Socket(ip, 8888);
@@ -34,6 +22,24 @@ public class Client {
                 File file = new File(filePath);
                 d.writeUTF(file.getName());
                 Files.copy(file.toPath(), d);
+            }
+            OutputStream os = s.getOutputStream();
+            os.write(msg.getBytes());
+            os.flush();
+            os.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+        }
+    }
+    public static void Send(String message, String ip,int socket) {
+
+        try {
+            String msg = "Iam waiting for message";
+            Socket s = new Socket(ip, socket);
+            BufferedOutputStream out = new BufferedOutputStream(s.getOutputStream());
+            try (DataOutputStream d = new DataOutputStream(out)) {
+                d.writeUTF(message);
             }
             OutputStream os = s.getOutputStream();
             os.write(msg.getBytes());
